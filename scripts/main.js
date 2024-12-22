5cb3e6e215bad5e3b9b7754d6ac024260ea9f865
lama-dev <techno.answer.io@gmail.com>
2024-12-22 16:54:23 +0300

    activities scroll and button animation

--- scripts/main.js
+++ scripts/main.js
@@ -12,17 +12,21 @@ document.getElementById('activity__top__button').addEventListener('click', funct
     activityExpanded = !activityExpanded;
 })
 
+
 function expandActivity() {
     let $activity = document.getElementById('activity');
     $activity.classList.add('expand_activity');
     $activity.classList.remove('shrink_activity');
+    turnHistoryButton();
 }
 
 function shrinkActivity() {
     let $activity = document.getElementById('activity');
     $activity.classList.add('shrink_activity');
     $activity.classList.remove('expand_activity');
+    turnHistoryButton();
 }
+
 
 function blurMain() {
     let $main = document.getElementById('main');
@@ -34,8 +38,25 @@ function blurMain() {
         activityExpanded = false;
     }, {once: true});
 }
+
 function revealMain() {
     let $main = document.getElementById('main');
     $main.classList.add('reveal_main');
     $main.classList.remove('blur_main');
+}
+
+
+isTurned = false
+function turnHistoryButton() {
+    let $historyButton = document.getElementById('activity-button');
+    
+    if (!isTurned) {
+        $historyButton.classList.add('turn-down');
+        $historyButton.classList.remove('turn-up');
+    } else {
+        $historyButton.classList.add('turn-up');
+        $historyButton.classList.remove('turn-down');
+    }
+
+    isTurned = !isTurned;
 }
